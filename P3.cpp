b5d992df5f6714aabaebcff297e8933ebca50bde
Rohan Jagtap <rohanjagtap1937@gmail.com>
2025-11-11 14:40:15 +0530

    Add files via upload

---
+++ P3.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+ int a,b,sum,Avg; 
+
+  cout<<"Enter A";
+  cin>>a;
+  cout<<"Enter B";
+  cin>>b;
+
+sum=(a+b);
+Avg=sum/2;
+
+cout<<"Sum="<<sum;
+cout<<"Average="<<Avg;
+
+return 0;
+}
